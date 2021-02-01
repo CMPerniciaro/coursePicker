dc66ae5e51dfe9ea865acf84766ab0cfd7bc1e1a
Carolyn Perniciaro <cmpvm5@mail.missouri.edu>
2021-02-01 10:47:26 -0600

    Course editor

--- components/CourseEditScreen.js
+++ components/CourseEditScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import * as Yup from 'yup';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Form from '../components/Form';
 
 const Field = ({label, value}) => {
   return (
@@ -9,6 +11,19 @@ const Field = ({label, value}) => {
     </View>
   );
 };
+const validationSchema = Yup.object().shape({
+  id: Yup.string()
+    .required()
+    .matches(/(F|W|S)\d{3,}/, 'Must be a term and 3-digit number')
+    .label('ID'),
+  meets: Yup.string()
+    .required()
+    .matches(/(M|Tu|W|Th|F)+ +\d\d?:\d\d-\d\d?:\d\d/, 'Must be weekdays followed by start and end time')
+    .label('Meeting times'),
+  title: Yup.string()
+    .required()
+    .label('Title'),
+});
 
 const CourseEditScreen = ({route}) => {
   const course = route.params.course;
@@ -16,9 +31,33 @@ const CourseEditScreen = ({route}) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <Field label="ID" value={course.id} />
-        <Field label="Meeting times" value={course.meets} />
-        <Field label="Title" value={course.title} />
+        <Form
+          initialValues={{
+            id: course.id,
+            meets: course.meets,
+            title: course.title,
+          }}
+          validationSchema={validationSchema}
+          >
+          <Form.Field
+              name="id"
+              leftIcon="identifier"
+              placeholder="F110"
+              autoCapitalize="none"
+              autoFocus={true}
+            />
+            <Form.Field
+              name="meets"
+              leftIcon="calendar-range"
+              placeholder="MThu 12:00-13:50"
+              autoCapitalize="none"
+            />
+            <Form.Field
+              name="title"
+              leftIcon="format-title"
+              placeholder="Introduction to programming"
+            />
+        </Form>
       </ScrollView>
     </SafeAreaView>
   );
